c25ddbcc38d345f6edc27063de0396b5ae5578c6
Kelly Santana <38592846+kehhsantana@users.noreply.github.com>
2024-05-27 22:46:22 -0300

    Initial project

---
+++ src/ calculator.test.js
@@ -0,0 +1,25 @@
+const { sum, subtraction, multiplication, division } = require('./calculator.js');
+
+describe('Calculator functions', () => {
+    test('adds 1 + 2 to equal 3', () => {
+        expect(sum(1, 2)).toBe(3);
+    });
+
+    test('subtracts 4 - 2 to equal 2', () => {
+        expect(subtraction(4, 2)).toBe(2);
+    });
+
+    test('multiplies 3 * 4 to equal 12', () => {
+        expect(multiplication(3, 4)).toBe(12);
+    });
+
+    test('divides 6 / 2 to equal 3', () => {
+        expect(division(6, 2)).toBe(3);
+    });
+
+    test('throws error when dividing by zero', () => {
+        expect(() => {
+            division(6, 0);
+        }).toThrow("Can't divide by zero");
+    });
+});
---
+++ src/calculator.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number} 
+ */
+function sum(a, b) {
+    return a + b;
+}
+
+/**
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number} 
+ */
+function subtraction(a, b) {
+    return a - b;
+}
+
+/**
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number} 
+ */
+function multiplication(a, b) {
+    return a * b;
+}
+
+/**
+ * @param {number} a
+ * @param {number} b 
+ * @returns {number} 
+ * @throws {Error} 
+ */
+function division(a, b) {
+    if (b === 0) {
+        throw new Error("Can't divide by zero");
+    }
+
+    return a / b;
+}
+
+module.exports = {
+    sum,
+    subtraction,
+    multiplication,
+    division,
+};
