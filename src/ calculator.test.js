0b85d2092d5464a93a4de6443098cce0099f6dbe
Kelly Santana <38592846+kehhsantana@users.noreply.github.com>
2024-06-26 21:21:15 -0300

    just sum function and test

--- src/ calculator.test.js
+++ src/ calculator.test.js
@@ -5,21 +5,21 @@ describe('Calculator functions', () => {
         expect(sum(1, 2)).toBe(3);
     });
 
-    test('subtracts 4 - 2 to equal 2', () => {
-        expect(subtraction(4, 2)).toBe(2);
-    });
+    // test('subtracts 4 - 2 to equal 2', () => {
+    //     expect(subtraction(4, 2)).toBe(2);
+    // });
 
-    test('multiplies 3 * 4 to equal 12', () => {
-        expect(multiplication(3, 4)).toBe(12);
-    });
+    // test('multiplies 3 * 4 to equal 12', () => {
+    //     expect(multiplication(3, 4)).toBe(12);
+    // });
 
-    test('divides 6 / 2 to equal 3', () => {
-        expect(division(6, 2)).toBe(3);
-    });
+    // test('divides 6 / 2 to equal 3', () => {
+    //     expect(division(6, 2)).toBe(3);
+    // });
 
-    test('throws error when dividing by zero', () => {
-        expect(() => {
-            division(6, 0);
-        }).toThrow("Can't divide by zero");
-    });
+    // test('throws error when dividing by zero', () => {
+    //     expect(() => {
+    //         division(6, 0);
+    //     }).toThrow("Can't divide by zero");
+    // });
 });
